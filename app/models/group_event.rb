992a4c456cbab370fa058865f0b3e025c6c0903e
Robert Zieliński <hello@robertz.co>
2020-10-12 11:20:06 +0200

    When group_event published presence validations

--- app/models/group_event.rb
+++ app/models/group_event.rb
@@ -4,4 +4,20 @@ class GroupEvent < ApplicationRecord
   STATE_TYPES = [DRAFT, PUBLISHED].freeze
 
   validates :state, inclusion: { in: STATE_TYPES }
+
+  with_options if: :published? do
+    validates :start, presence: true
+    validates :end, presence: true
+    validates :duration, presence: true
+
+    validates :name, presence: true
+    validates :description, presence: true
+    validates :location, presence: true
+  end
+
+  # State methods
+
+  def published?
+    state == PUBLISHED
+  end
 end
--- spec/models/group_event_spec.rb
+++ spec/models/group_event_spec.rb
@@ -3,5 +3,17 @@ require 'rails_helper'
 describe GroupEvent, type: :model do
   describe '#validations' do
     it { should validate_inclusion_of(:state).in_array(described_class::STATE_TYPES) }
+
+    context 'when state is published' do
+      subject { build :group_event, :published }
+
+      it { should validate_presence_of(:start) }
+      it { should validate_presence_of(:end) }
+      it { should validate_presence_of(:duration) }
+
+      it { should validate_presence_of(:name) }
+      it { should validate_presence_of(:description) }
+      it { should validate_presence_of(:location) }
+    end
   end
 end
