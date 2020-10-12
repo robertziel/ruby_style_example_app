71c5e417eb063da438a13be9bfbfa63393ba7a4d
Robert Zieliński <hello@robertz.co>
2020-10-12 11:11:06 +0200

    Use factory_bot_rails gem instead of factory_bot

--- spec/factories/group_event.rb
+++ spec/factories/group_event.rb
@@ -1,4 +1,11 @@
 FactoryBot.define do
   factory :group_event do
+    trait :draft do
+      state { GroupEvent::DRAFT }
+    end
+
+    trait :published do
+      state { GroupEvent::PUBLISHED }
+    end
   end
 end
