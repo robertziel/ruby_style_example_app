e346ddc7a9803fa0b9d1cebf8e44cbc6d375fb22
Robert Zieliński <hello@robertz.co>
2020-10-12 10:50:26 +0200

    Fix, group_event state inclusion validation spec by adding should

--- spec/models/group_event_spec.rb
+++ spec/models/group_event_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 describe GroupEvent, type: :model do
   describe '#validations' do
-    it { validate_inclusion_of(:state).in_array(described_class::STATE_TYPES) }
+    it { should validate_inclusion_of(:state).in_array(described_class::STATE_TYPES) }
   end
 end
